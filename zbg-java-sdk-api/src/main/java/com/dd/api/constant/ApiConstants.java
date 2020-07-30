d40fccebdeff071e33a0fc9fb231d0febecf2cd6
ZBG -全球前十交易所 <49393763+zbgapi@users.noreply.github.com>
2020-07-30 18:40:06 +0800

    Update ApiConstants.java
    更新域名

--- zbg-java-sdk-api/src/main/java/com/dd/api/constant/ApiConstants.java
+++ zbg-java-sdk-api/src/main/java/com/dd/api/constant/ApiConstants.java
@@ -9,15 +9,15 @@ public interface ApiConstants {
     /**
      * zbg 默认访问域名
      */
-    String DEFAULT_ENDPOINT = "https://www.zbgpro.net";
+    String DEFAULT_ENDPOINT = "https://www.zbg.live";
     /**
      * zbg K线 默认访问域名
      */
-    String DEFAULT_KLINE_ENDPOINT = "https://kline.zbgpro.net";
+    String DEFAULT_KLINE_ENDPOINT = "https://kline.zbg.live";
     /**
      * zbg 默认 ws 地址
      */
-    String DEFAULT_WS_ENDPOINT = "wss:/kline.zbgpro.net/websocket";
+    String DEFAULT_WS_ENDPOINT = "wss:/kline.zbg.live/websocket";
 
     /**
      * The default timeout is 30 seconds.
